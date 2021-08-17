c52b512c2c057f0ac5614a6787f5cda9376bf375
andriv52 <89083876+andriv52@users.noreply.github.com>
2021-08-17 16:30:03 +0300

    Add files via upload

---
+++ lang.js
@@ -0,0 +1,26 @@
+const langArr = {
+    "unit" :  {
+        "ru": "создаем многоязычный сайт",
+        "en": "build a multi language site ",
+    }, 
+    "chip": {
+        "ru": "графический чип",
+        "en": "graphics processing unit",
+    }, 
+    "memory": {
+        "ru": "объем памяти",
+        "en": "memory size",
+    }, 
+    "memory-type": {
+        "ru": "тип памяти",
+        "en": "memory type",
+    }, 
+    "cool": {
+        "ru": "тип системы охлаждения",
+        "en": "сooling system type",
+    }, 
+    "more": {
+        "ru": "детально о товаре",
+        "en": "more details",
+    }, 
+}
